cf72296ec6110b1405a4717906088eca74d1176e
Robin Simonneau <robin.simonneau@robin-sim.fr>
2023-04-26 10:59:15 +0200

    V1 of the plugin

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Tiny-directory
+Plugin URI: https://wpusermanager.com/
+Plugin URI: https://wpusermanager.com/
+Description: ! Nécéssite le plugin WP User Manager ! Récupère les utilisateurs dans la base de donnée et affiches leurs informations sous la forme d'un annuaire dépliable
+Author: Robin Simonneau
+Version: 1.0
+Author URI: https://robin-sim.fr/
+*/
+
+/**
+ * Récupère les utilisateurs dans la base de donnée et les affect à un tableau HTML
+ * @return string Le tableau HTML
+ */
+
+add_shortcode('users_directory','createDirectoryFromDBUsers');
+
+
+function createDirectoryFromDBUsers(): string{
+    //Ajout de la feuille de style et du javascript
+    wp_enqueue_style('tiny-directory-css',plugins_url('tiny-directory.css',__FILE__));
+    wp_enqueue_script('tiny-directory-js',plugins_url('tiny-directory.js',__FILE__),array(), false, true);
+    $users = get_users();
+    // Vérifier s'il y a des utilisateurs
+    if ( !empty( $users ) ) {
+        //Génère le tableau
+        $html = <<<HTML
+    <table class="tiny-directory-table">
+    <thead >
+        <tr class="tiny-directory-tr">
+            <th class="tiny-directory-th" colspan="1">NOM / Prénom</th>
+            <th class="tiny-directory-th" colspan="1">Email</th> 
+            <th class="tiny-directory-th" colspan="1">Téléphone</th> 
+            <th class="tiny-directory-th" colspan="1">Fonction</th> 
+
+    </thead>
+    <tbody>
+    HTML;
+        foreach ( $users as $user ) {
+            $userID = $user->ID;
+            $userAvatar = get_avatar($userID);
+            $linkToProfilePage = home_url()."/profile/$user->user_nicename";
+            $html.= <<<HTML
+        <tr class="user-$userID tiny-directory-tr">
+            <td class="no-display-fields" id="pp-$userID" data-id="$userID">$userAvatar</td>
+            <td class="no-display-fields" id="login-$userID">$linkToProfilePage</td>
+            <td class="tiny-directory-td name-$userID">$user->display_name</td>
+            <td class="tiny-directory-td email-$userID"><a href="mailto:$user->user_email">$user->user_email</td>
+            <td class="tiny-directory-td phone-$userID">E.T téléphone maison</td>
+            <td class="tiny-directory-td position-$userID">Fonction</td>
+        </tr>
+HTML;
+        }
+        $html.= <<<HTML
+    </tbody>
+</table>
+
+HTML;
+        return $html;
+    } else {
+        return "Error no users has been found :(";
+    }
+}
+
+
---
+++ tiny-directory.js
@@ -0,0 +1,69 @@
+console.log("Tiny-directory is load")
+
+//On récupère le tableau
+const rows = document.querySelectorAll('tbody tr');
+//Pour chaque ligne on ajoute un élément caché qui contient le document
+rows.forEach((row) => {
+    const userId = row.querySelector('[data-id]').getAttribute('data-id');
+    const detailsRow = document.createElement('tr');
+    detailsRow.classList.add('details');
+
+    const detailsCell = document.createElement('td');
+    detailsCell.setAttribute('colspan', '3');
+
+    //Récupération du lien vers la page de profile
+    const linkToProfilePage = row.querySelector(`#login-${userId}`).textContent;
+
+
+    //Récupération de l'image de profile caché
+    const profilePicture = document.querySelector(`#pp-${userId}>img`)
+    const ppCopy = profilePicture.cloneNode(true);
+
+    //Récupération des informations déjà affichés
+    console.log(`.tiny-directory-td email-${userId}`)
+    const displayName = "Nom : "+row.querySelector(`.name-${userId}`).textContent;
+    const email = "Email : "+row.querySelector(`.email-${userId}`).textContent;
+    const phone = "phone"//row.querySelector(`.phone-${userId}`).textContent;
+    const position = "position"//row.querySelector(`.position-${userId}`).textContent;
+
+    //Créations des élements affiché dans le bloc caché
+    const displayNameP = document.createElement('p');
+    displayNameP.textContent = displayName;
+
+    const emailP = document.createElement('p');
+    emailP.textContent = email;
+
+    const phoneP = document.createElement('p');
+    phoneP.textContent = phone;
+
+    const positionP = document.createElement('p');
+    positionP.textContent = position;
+
+    const linkToProfilePageA = document.createElement('a');
+    linkToProfilePageA.textContent = "Page de profile";
+    linkToProfilePageA.href = linkToProfilePage
+
+    const detailsContent = document.createElement('div');
+    detailsContent.classList.add('detail-div-flex');
+    detailsContent.appendChild(ppCopy);
+
+    const detailsTextContent = document.createElement('div')
+    detailsTextContent.classList.add('detail-div-flex-text');
+
+    detailsTextContent.appendChild(displayNameP);
+    detailsTextContent.appendChild(emailP);
+    //detailsTextContent.appendChild(phone);
+    //detailsTextContent.appendChild(position);
+    detailsTextContent.appendChild(linkToProfilePageA);
+
+    detailsContent.appendChild(detailsTextContent);
+
+    detailsCell.appendChild(detailsContent);
+    detailsRow.appendChild(detailsCell);
+    row.insertAdjacentElement('afterend', detailsRow);
+
+    //On écoute le clique sur chaque ligne, puis on affiche si l'élément est cliqué
+    row.addEventListener('click', () => {
+        detailsRow.classList.toggle('active');
+    });
+});
